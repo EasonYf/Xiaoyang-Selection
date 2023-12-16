ee25cf70197bd62120d956b5a56669d7653359e8
Eason <1547837791@qq.com>
2023-12-16 20:33:22 +0800

    完成数据大屏的基础搭建

---
+++ src/views/screen/components/top/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="top">
+    <div class="left">
+      <span class="lbtn" @click="goHome">首页</span>
+    </div>
+    <div class="center"><span>智慧旅游可视化大数据平台</span></div>
+    <div class="right">
+      <span class="rbtn">数据报告</span
+      ><span class="time">当前时间:2023年12月16日</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+//获得路由器的组件实例
+let $router = useRouter();
+
+//跳转首页按钮的回调函数
+const goHome = () => {
+  $router.push('/home')
+}
+</script>
+<script lang="ts">
+export default {
+  name: "ScreenTop",
+};
+</script>
+<style scoped lang="scss">
+.top {
+  display: flex;
+  width: 100%;
+  height: 40px;
+  color: #29fcff;
+  .left {
+    flex: 1.5;
+    display: flex;
+    flex-direction: row-reverse;
+    background: url("../../images/dataScreen-header-left-bg.png") no-repeat;
+    background-size: cover;
+    .lbtn {
+      width: 150px;
+      height: 40px;
+      line-height: 40px;
+      font-size: 20px;
+      text-align: center;
+      cursor: pointer;
+      background: url("../../images/dataScreen-header-btn-bg-l.png") no-repeat;
+    }
+  }
+  .right {
+    flex: 1.5;
+    display: flex;
+    justify-content: space-between;
+    background: url("../../images/dataScreen-header-right-bg.png") no-repeat;
+    text-align: center;
+    background-size: cover;
+    .rbtn {
+      width: 150px;
+      height: 40px;
+      line-height: 40px;
+      font-size: 20px;
+      cursor: pointer;
+      background: url("../../images/dataScreen-header-btn-bg-r.png") no-repeat;
+    }
+  }
+  .center {
+    width: 100%;
+    height: 72px;
+    flex: 2;
+    text-align: center;
+    font-size: 40px;
+    background: url("../../images/dataScreen-header-center-bg.png") no-repeat;
+    background-size: 100% 100%;
+    vertical-align: bottom;
+  }
+}
+</style>
--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,13 +1,56 @@
 <template>
-  <div>
-    <h1>这是数字大屏</h1>
+  <div class="container">
+    <!-- 数据大屏 -->
+    <!-- 上下结构 -->
+    <div class="screen" ref="screen">
+      <div class="top"><Top></Top></div>
+      <div class="buttom">
+        <div class="left">left</div>
+        <div class="center">center</div>
+        <div class="right">right</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref , onMounted} from 'vue';
+import Top from './components/top/index.vue'
+// 获取screen组件实例
+let screen = ref()
 
+
+let getRatio = (w=1920,h=1080) => {
+  let ww = window.innerWidth / w
+  let hh = window.innerHeight / h
+  return ww < hh ? ww : hh
+}
+window.onresize = () => {
+  screen.value.style.transform =`scale(${getRatio()}) translate(-50%,-50%)`
+}
+
+onMounted(()=>{
+  screen.value.style.transform =`scale(${getRatio()}) translate(-50%,-50%)`
+})
 </script>
-
-<style scoped>
-
+<script lang="ts">
+export default {
+  name:'DataScreen'
+}
+</script>
+<style scoped lang="scss">
+.container {
+  width: 100vw;
+  height: 100vh;
+  background: url("./images/bg.png") no-repeat;
+  background-size: cover;
+  .screen{
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    width: 1920px;
+    height: 1080px;
+    transform-origin: left top;
+  }
+}
 </style>
